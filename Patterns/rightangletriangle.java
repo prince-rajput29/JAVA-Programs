377c0116247c379d29d834078ca0b3b75d2e34da
prince-rajput29 <148304290+prince-rajput29@users.noreply.github.com>
2023-11-06 11:27:23 +0530

    Add files via upload

---
+++ Patterns/rightangletriangle.java
@@ -0,0 +1,15 @@
+public class rightangletriangle {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=5-i;j>0;j--){
+                System.out.print("*");
+            }
+            for(int j=0;j<5;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+    
+}
+    }
+    
+}
